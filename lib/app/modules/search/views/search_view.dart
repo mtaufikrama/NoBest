2c50c6ca3052957c4942f5f561be56041ab077b5
mtaufikrama <mtaufikrama14@gmail.com>
2023-08-22 23:43:31 +0700

    hapus tutor search

--- lib/app/modules/search/views/search_view.dart
+++ lib/app/modules/search/views/search_view.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:nobes/app/data/model/usda_list.dart';
@@ -10,35 +8,10 @@ import 'package:nobes/app/data/services/public.dart';
 import 'package:nobes/app/data/services/translate.dart';
 import 'package:nobes/app/data/widget/refresh_page.dart';
 import 'package:nobes/app/routes/app_pages.dart';
-import 'package:tutorial_coach_mark/tutorial_coach_mark.dart';
-import '../../../data/model/tutorial.dart';
-import '../../../data/services/getstorages.dart';
 import '../controllers/search_controller.dart';
 
-class SearchView extends StatefulWidget {
+class SearchView extends GetView<SearchsController> {
   const SearchView({super.key});
-
-  @override
-  State<SearchView> createState() => _SearchViewState();
-}
-
-class _SearchViewState extends State<SearchView> {
-  late TutorialCoachMark tutorialCoachMark;
-
-  GlobalKey keySearch = GlobalKey();
-  GlobalKey keySend = GlobalKey();
-  GlobalKey keyHistory = GlobalKey();
-
-  final controller = Get.put(SearchsController());
-
-  @override
-  void initState() {
-    if (controller.getTutorial.value.search != true) {
-      createTutorial();
-      Future.delayed(Duration.zero, showTutorial);
-    }
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -53,7 +26,7 @@ class _SearchViewState extends State<SearchView> {
       ),
       body: RefreshPage(
         routes: Routes.SEARCH,
-        child: Column(
+        child: ListView(
           children: [
             const SizedBox(
               height: 10,
@@ -61,37 +34,27 @@ class _SearchViewState extends State<SearchView> {
             Row(
               children: [
                 Expanded(
-                  child: Stack(
-                    children: [
-                      SizedBox(
-                        key: keySearch,
-                        width: Get.width,
-                        height: 100,
-                      ),
-                      TextFormField(
-                        autofocus: true,
-                        controller: controller.searchController,
-                        decoration: InputDecoration(
-                          label: teksLanguage(
-                            "Search Food",
-                            style: Font.regular(
-                              color: Colors.grey,
-                            ),
-                          ),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(15),
-                            borderSide: const BorderSide(
-                              color: Warna.primary,
-                            ),
-                          ),
+                  child: TextFormField(
+                    autofocus: true,
+                    controller: controller.searchController,
+                    decoration: InputDecoration(
+                      label: teksLanguage(
+                        "Search Food",
+                        style: Font.regular(
+                          color: Colors.grey,
                         ),
-                        onEditingComplete: controller.onSubmit,
                       ),
-                    ],
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                        borderSide: const BorderSide(
+                          color: Warna.primary,
+                        ),
+                      ),
+                    ),
+                    onEditingComplete: controller.onSubmit,
                   ),
                 ),
                 IconButton(
-                  key: keySend,
                   onPressed: controller.onSubmit,
                   icon: const ImageIcon(
                     AssetImage(
@@ -101,132 +64,38 @@ class _SearchViewState extends State<SearchView> {
                 ),
               ],
             ),
-            Expanded(
-              key: keyHistory,
-              child: Obx(() {
-                return ListView.builder(
-                  itemCount: controller.getSearch.length,
-                  reverse: true,
-                  itemBuilder: (context, index) {
-                    return InkWell(
-                      onTap: () async {
-                        await controller.onSearch(controller.getSearch[index]);
-                      },
-                      child: Card(
-                        child: Container(
-                          height: 50,
-                          alignment: Alignment.center,
-                          child: Text(
-                            controller.getSearch[index].toString(),
-                            style: Font.regular(
-                              fontSize: 15.0,
-                              color: Warna.baseWhite,
-                            ),
+            Obx(() {
+              return ListView.builder(
+                itemCount: controller.getSearch.length,
+                reverse: true,
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemBuilder: (context, index) {
+                  return InkWell(
+                    onTap: () async {
+                      await controller.onSearch(controller.getSearch[index]);
+                    },
+                    child: Card(
+                      child: Container(
+                        height: 50,
+                        alignment: Alignment.center,
+                        child: Text(
+                          controller.getSearch[index].toString(),
+                          style: Font.regular(
+                            fontSize: 15.0,
+                            color: Warna.baseWhite,
                           ),
                         ),
                       ),
-                    );
-                  },
-                );
-              }),
-            ),
+                    ),
+                  );
+                },
+              );
+            }),
           ],
         ),
       ),
     );
-  }
-
-  void showTutorial() {
-    tutorialCoachMark.show(context: context);
-  }
-
-  void createTutorial() {
-    tutorialCoachMark = TutorialCoachMark(
-      targets: _createTargets(),
-      textStyleSkip: Font.regular(),
-      textSkip: 'SKIP >>',
-      paddingFocus: 10,
-      opacityShadow: 0.5,
-      imageFilter: ImageFilter.blur(sigmaX: 8, sigmaY: 8),
-      onFinish: () async {
-        await Storages.setTutorial(tutorial: TutorialModel(search: true));
-      },
-      onSkip: () async {
-        await Storages.setTutorial(tutorial: TutorialModel(search: true));
-      },
-    );
-  }
-
-  List<TargetFocus> _createTargets() {
-    List<TargetFocus> targets = [];
-    targets.add(
-      TargetFocus(
-        identify: "Search",
-        keyTarget: keySearch,
-        shape: ShapeLightFocus.RRect,
-        alignSkip: Alignment.topRight,
-        color: Warna.primary,
-        contents: [
-          TargetContent(
-            align: ContentAlign.bottom,
-            builder: (context, controller) {
-              return teksLanguage(
-                "Profile photo:\nTo display the user's photo.",
-                style: Font.regular(
-                  color: Colors.white,
-                ),
-              );
-            },
-          ),
-        ],
-      ),
-    );
-    targets.add(
-      TargetFocus(
-        identify: "Send",
-        keyTarget: keySend,
-        color: Warna.primary,
-        alignSkip: Alignment.topRight,
-        enableOverlayTab: true,
-        contents: [
-          TargetContent(
-            align: ContentAlign.top,
-            builder: (context, controller) {
-              return teksLanguage(
-                "Profile Data:\nDisplaying user data that has been inputted on the data input page.",
-                style: Font.regular(
-                  color: Colors.white,
-                ),
-              );
-            },
-          ),
-        ],
-      ),
-    );
-    targets.add(
-      TargetFocus(
-        identify: "History",
-        keyTarget: keyHistory,
-        shape: ShapeLightFocus.RRect,
-        color: Warna.primary,
-        alignSkip: Alignment.topRight,
-        enableOverlayTab: true,
-        contents: [
-          TargetContent(
-            align: ContentAlign.top,
-            builder: (context, controller) {
-              return teksLanguage(
-                "Back To Home Page",
-                style: Font.regular(
-                  color: Colors.white,
-                ),
-              );
-            },
-          ),
-        ],
-      ),
-    );
-    return targets;
   }
 }
 
